36916af7112be9aa78aded0c48ae0d1d640bf665
Weiglas <weiglasribeiro@gmail.com>
2020-01-17 12:48:21 -0300

    Initial commit

---
+++ get-export-env-points/Commands.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+
+using AcAp = Autodesk.AutoCAD.ApplicationServices.Application;
+
+namespace AcadPlugin
+{
+    public class Commands
+    {
+        [CommandMethod("TEST")]
+        public void Test()
+        {
+
+            Document doc = AcAp.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+            Editor ed = doc.Editor;
+            using (var tr = db.TransactionManager.StartTransaction())
+            {
+                ed.WriteMessage("funcionou");
+                tr.Commit();
+            }
+        }
+    }
+}
